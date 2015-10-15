c4f34dcc101f46c3b5eee4bceccee5ad2ef043bf
study <549761863@qq.com>
2015-10-15 20:30:25 +0800

    inc文件提交

---
+++ newtravel/application/logs/2015/10/15.php
@@ -0,0 +1,35 @@
+<?php defined('SYSPATH') OR die('No direct script access.'); ?>
+
+2015-10-15 20:29:00 --- ERROR: HTTP_Exception_404 [ 404 ]: The requested URL public/images/login-logo.png was not found on this server. ~ SYSPATH\classes\kohana\request\client\internal.php [ 87 ]
+2015-10-15 20:29:00 --- STRACE: HTTP_Exception_404 [ 404 ]: The requested URL public/images/login-logo.png was not found on this server. ~ SYSPATH\classes\kohana\request\client\internal.php [ 87 ]
+--
+#0 D:\phpStudy\WWW\st\newtravel\system\classes\kohana\request\client.php(64): Kohana_Request_Client_Internal->execute_request(Object(Request))
+#1 D:\phpStudy\WWW\st\newtravel\system\classes\kohana\request.php(1158): Kohana_Request_Client->execute(Object(Request))
+#2 D:\phpStudy\WWW\st\newtravel\index.php(127): Kohana_Request->execute()
+#3 {main}
+2015-10-15 20:29:10 --- ERROR: HTTP_Exception_404 [ 404 ]: The requested URL public/images/logo.png was not found on this server. ~ SYSPATH\classes\kohana\request\client\internal.php [ 87 ]
+2015-10-15 20:29:10 --- STRACE: HTTP_Exception_404 [ 404 ]: The requested URL public/images/logo.png was not found on this server. ~ SYSPATH\classes\kohana\request\client\internal.php [ 87 ]
+--
+#0 D:\phpStudy\WWW\st\newtravel\system\classes\kohana\request\client.php(64): Kohana_Request_Client_Internal->execute_request(Object(Request))
+#1 D:\phpStudy\WWW\st\newtravel\system\classes\kohana\request.php(1158): Kohana_Request_Client->execute(Object(Request))
+#2 D:\phpStudy\WWW\st\newtravel\index.php(127): Kohana_Request->execute()
+#3 {main}
+2015-10-15 20:29:11 --- ERROR: HTTP_Exception_404 [ 404 ]: The requested URL public/images/logo.png was not found on this server. ~ SYSPATH\classes\kohana\request\client\internal.php [ 87 ]
+2015-10-15 20:29:11 --- STRACE: HTTP_Exception_404 [ 404 ]: The requested URL public/images/logo.png was not found on this server. ~ SYSPATH\classes\kohana\request\client\internal.php [ 87 ]
+--
+#0 D:\phpStudy\WWW\st\newtravel\system\classes\kohana\request\client.php(64): Kohana_Request_Client_Internal->execute_request(Object(Request))
+#1 D:\phpStudy\WWW\st\newtravel\system\classes\kohana\request.php(1158): Kohana_Request_Client->execute(Object(Request))
+#2 D:\phpStudy\WWW\st\newtravel\index.php(127): Kohana_Request->execute()
+#3 {main}
+2015-10-15 20:29:12 --- ERROR: ErrorException [ 1 ]: Class 'SoapClient' not found ~ APPPATH\classes\model\upgrade.php [ 22 ]
+2015-10-15 20:29:12 --- STRACE: ErrorException [ 1 ]: Class 'SoapClient' not found ~ APPPATH\classes\model\upgrade.php [ 22 ]
+--
+#0 [internal function]: Kohana_Core::shutdown_handler('new')
+#1 {main}
+2015-10-15 20:29:13 --- ERROR: HTTP_Exception_404 [ 404 ]: The requested URL public/images/index_logo.png was not found on this server. ~ SYSPATH\classes\kohana\request\client\internal.php [ 87 ]
+2015-10-15 20:29:13 --- STRACE: HTTP_Exception_404 [ 404 ]: The requested URL public/images/index_logo.png was not found on this server. ~ SYSPATH\classes\kohana\request\client\internal.php [ 87 ]
+--
+#0 D:\phpStudy\WWW\st\newtravel\system\classes\kohana\request\client.php(64): Kohana_Request_Client_Internal->execute_request(Object(Request))
+#1 D:\phpStudy\WWW\st\newtravel\system\classes\kohana\request.php(1158): Kohana_Request_Client->execute(Object(Request))
+#2 D:\phpStudy\WWW\st\newtravel\index.php(127): Kohana_Request->execute()
+#3 {main}
