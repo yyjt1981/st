3197791cc38723f0c28955143ecc14c7d414f133
study <549761863@qq.com>
2015-10-17 13:09:42 +0800

    不确定

--- newtravel/application/logs/2015/10/15.php
+++ newtravel/application/logs/2015/10/15.php
@@ -33,3 +33,22 @@
 #1 D:\phpStudy\WWW\st\newtravel\system\classes\kohana\request.php(1158): Kohana_Request_Client->execute(Object(Request))
 #2 D:\phpStudy\WWW\st\newtravel\index.php(127): Kohana_Request->execute()
 #3 {main}
+2015-10-15 21:34:08 --- ERROR: HTTP_Exception_404 [ 404 ]: The requested URL public/images/logo.png was not found on this server. ~ SYSPATH\classes\kohana\request\client\internal.php [ 87 ]
+2015-10-15 21:34:08 --- STRACE: HTTP_Exception_404 [ 404 ]: The requested URL public/images/logo.png was not found on this server. ~ SYSPATH\classes\kohana\request\client\internal.php [ 87 ]
+--
+#0 D:\phpStudy\WWW\st\newtravel\system\classes\kohana\request\client.php(64): Kohana_Request_Client_Internal->execute_request(Object(Request))
+#1 D:\phpStudy\WWW\st\newtravel\system\classes\kohana\request.php(1158): Kohana_Request_Client->execute(Object(Request))
+#2 D:\phpStudy\WWW\st\newtravel\index.php(127): Kohana_Request->execute()
+#3 {main}
+2015-10-15 21:34:09 --- ERROR: HTTP_Exception_404 [ 404 ]: The requested URL public/images/index_logo.png was not found on this server. ~ SYSPATH\classes\kohana\request\client\internal.php [ 87 ]
+2015-10-15 21:34:09 --- STRACE: HTTP_Exception_404 [ 404 ]: The requested URL public/images/index_logo.png was not found on this server. ~ SYSPATH\classes\kohana\request\client\internal.php [ 87 ]
+--
+#0 D:\phpStudy\WWW\st\newtravel\system\classes\kohana\request\client.php(64): Kohana_Request_Client_Internal->execute_request(Object(Request))
+#1 D:\phpStudy\WWW\st\newtravel\system\classes\kohana\request.php(1158): Kohana_Request_Client->execute(Object(Request))
+#2 D:\phpStudy\WWW\st\newtravel\index.php(127): Kohana_Request->execute()
+#3 {main}
+2015-10-15 21:34:09 --- ERROR: ErrorException [ 1 ]: Class 'SoapClient' not found ~ APPPATH\classes\model\upgrade.php [ 22 ]
+2015-10-15 21:34:09 --- STRACE: ErrorException [ 1 ]: Class 'SoapClient' not found ~ APPPATH\classes\model\upgrade.php [ 22 ]
+--
+#0 [internal function]: Kohana_Core::shutdown_handler('new')
+#1 {main}
