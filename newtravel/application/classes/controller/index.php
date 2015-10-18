51d69ddf9a8d527e7dc306150c937e48a6e40da5
yyjt1981 <yyjt1981@sina.com>
2015-10-18 20:31:09 +0800

    同步文章，产品，团购产品到search表。

--- newtravel/application/classes/controller/index.php
+++ newtravel/application/classes/controller/index.php
@@ -88,12 +88,11 @@ class Controller_Index extends Stourweb_Controller
     }
 
     //同步search表
-    //TODO： 同步攻略，团购
     public function sync_search()
     {
-        $line_list = ORM::factory('line')->where('ishidden', '=', 0)->find_all();
         DB::query(Database::DELETE, 'delete from sline_search')->execute();
 
+        $line_list = ORM::factory('line')->where('ishidden', '=', 0)->find_all();
         foreach ($line_list as $line) {
             $model = ORM::factory('search');
             $model->webid = $line->webid;
@@ -103,6 +102,34 @@ class Controller_Index extends Stourweb_Controller
             $model->description = '';
             $model->litpic = $line->linepic;
             $model->shownum = $line->shownum;
+            $model->save();
+        }
+
+        //团购
+        $tuan_list = ORM::factory('tuan')->where('ishidden', '=', 0)->find_all();
+        foreach ($tuan_list as $tuan) {
+            $model = ORM::factory('search');
+            $model->webid = $tuan->webid;
+            $model->aid = $tuan->aid;
+            $model->typeid = 13;
+            $model->title = $tuan->title;
+            $model->description = $tuan->description;
+            $model->litpic = $tuan->litpic;
+            $model->shownum = $tuan->shownum;
+            $model->save();
+        }
+
+        //游记·攻略
+        $article_list = ORM::factory('article')->where('ishidden', '=', 0)->find_all();
+        foreach ($article_list as $article) {
+            $model = ORM::factory('search');
+            $model->webid = $article->webid;
+            $model->aid = $article->aid;
+            $model->typeid = 4;
+            $model->title = $article->articlename;
+            $model->description = $article->description;
+            $model->litpic = $article->litpic;
+            $model->shownum = $article->shownum;
             $model->save();
         }
     }
